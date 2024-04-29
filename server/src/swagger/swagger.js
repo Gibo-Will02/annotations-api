4f5d333128282553337224182c4970df74140397
nlillich <89368743+nlillich@users.noreply.github.com>
2024-04-29 18:53:42 +0000

    Added swagger documentation

---
+++ server/src/swagger/swagger.js
@@ -0,0 +1,31 @@
+const { Express, Request, Response } = require('express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const { version } = require('../../package.json');
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+          title: 'Perusall API Documentation',
+          version: '1.0.0',
+          description: 'API documentation for the Perusall API',
+        },
+      },
+    apis: ["./src/routers/api.js"],
+  };
+
+const swaggerSpec = swaggerJsdoc(options);
+
+function swaggerDocs(app, num) {
+    app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+    app.get("/doc.json", (Request, Response) => {
+        Request.setHeader("Content-Type", "application/json");
+        Response.send(swaggerSpec);
+    });
+
+    console.log('plz work');
+}
+
+module.exports = swaggerDocs;
